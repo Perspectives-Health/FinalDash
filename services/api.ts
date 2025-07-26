c3cd730e1708da32ea93e0deb285cfab588d723b
nujesse1 <jesse@aicavalier.com>
2025-07-26 10:21:47 -0700

    Update api.ts

--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,7 @@
 import { auth } from '@/lib/auth'
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_ENV === 'prod' 
-  ? "https://165.227.120.106" 
+  ? "http://165.227.120.106" 
   : "http://localhost"
 // Debug: Log the API base URL
 console.log("API_BASE_URL:", API_BASE_URL)
